edc9a5fd64d9776ed1cebefecb314fc84f972b16
Daan-03 <palsdaan@outlook.com>
2025-06-09 14:55:23 +0200

    Door+Laser

---
+++ Assets/Scripts/LaserCycle.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class SpriteCycler : MonoBehaviour
+{
+    public Sprite[] sprites;
+    private SpriteRenderer spriteRenderer;
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (sprites.Length > 0)
+        {
+            spriteRenderer.sprite = sprites[currentIndex];
+            StartCoroutine(CycleSprites());
+        }
+    }
+
+    IEnumerator CycleSprites()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f); // Wait for 5 seconds
+
+            currentIndex = (currentIndex + 1) % sprites.Length;
+            spriteRenderer.sprite = sprites[currentIndex];
+        }
+    }
+}
